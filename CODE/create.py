ed74b4cd0afd56a77d7c66857a874d40542c1115
Ryan <hodgsonr1234@gmail.com>
2019-05-01 22:04:00 +0800

    Added in seed

--- CODE/create.py
+++ CODE/create.py
@@ -1,7 +1,7 @@
 from networkx import nx
 
-def ER_graph(N, p):
-    return nx.erdos_renyi_graph(N, p)
+def ER_graph(N, p, seed):
+    return nx.erdos_renyi_graph(N, p, seed)
 
-def SF_graph(N):
+def SF_graph(N, seed):
     return nx.scale_free_graph(N).to_undirected()
