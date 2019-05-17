4ad5b9fed7b434c662a031075e05ca878b3e7401
Ryan <hodgsonr1234@gmail.com>
2019-05-17 12:07:10 +0800

    Clean up - SF graph issue has been addressed

--- CODE/experiment_one_extn.py
+++ CODE/experiment_one_extn.py
@@ -4,6 +4,61 @@ import delete
 import get
 import numpy as np
 
+def init():
+    facebook = read_graph('facebook_combined.txt.gz')
+    fb_connected_nodes = get_most_connected_nodes(facebook)
+    fb_connected_nodes = fb_connected_nodes[20:30]
+
+    diameters = []
+    recursive_attack(facebook, fb_connected_nodes, diameters)
+    print(diameters)
+
+def recursive_attack(G, connected_nodes, diameters):
+        
+    if len(connected_nodes) == 0:
+        print("RETURNING NOW....")
+        print(diameters)
+        return diameters
+    
+    print("-----------------------")
+    (node, _) = connected_nodes.pop(0)
+    print("NODE : " + str(node))
+    print("NO. OF NODES BEFORE : " + str(len(G.nodes())))
+    G.remove_node(node)
+    print("NO. OF NODES AFTER : " + str(len(G.nodes())))
+    L = estimate_path_length(G)
+    print("L : " + str(L))
+    diameters.append(L) 
+    print(diameters)
+
+    recursive_attack(G, connected_nodes, diameters)
+
+
+# average path length
+def sample_path_lengths(G, trials=1000):
+    nodes = list(G)
+
+    pairs = np.random.choice(nodes, (trials, 2)) 
+
+    lengths = [nx.shortest_path_length(G, *pair) 
+               for pair in pairs]
+    
+    return lengths
+
+# averages the average path length
+def estimate_path_length(G, trials=1000):
+    return np.mean(sample_path_lengths(G, trials))
+
+def remove_node(G, n):
+    G_local = G.copy()
+    G_local.remove_node(n)
+    return G_local
+
+def get_most_connected_nodes(G):
+    degrees = list(G.degree)
+    degrees.sort(key=lambda tup: tup[1], reverse=True)
+    return degrees    
+
 # reads in file and generates a graph line by line
 def read_graph(filename):
     G = nx.Graph()
@@ -11,119 +66,5 @@ def read_graph(filename):
     G.add_edges_from(array)
     return G
 
-def nodes_connected(G, u, v):
-    return u in G.neighbors(v)
 
-# average path length
-def sample_path_lengths(G, nodes=None, trials=1000):
-    if nodes is None:
-        nodes = list(G)
-    else:
-        nodes = list(nodes)
-        
-    pairs = np.random.choice(nodes, (trials, 2)) 
-    
-    print(len(pairs))
-
-    connected_pairs = []
-    for pair in pairs:
-        [u, v] = pair
-        if nodes_connected(G, u, v):
-            connected_pairs.append(pair)
-
-    print(len(connected_pairs))
-
-    lengths = [nx.shortest_path_length(G, *pair) 
-               for pair in pairs]
-    
-
-
-    # lengths = []
-    # for pair in pairs:
-    #     [u, v] = pair
-    #     if nodes_connected(G, u, v):
-    #         lengths.append(nx.shortest_path_length(G, *pair))
-    
-    return lengths
-
-# averages the average path length
-def estimate_path_length(G, nodes=None, trials=1000):
-    return np.mean(sample_path_lengths(G, nodes, trials))
-
-# deletes random nodes
-def random_nodes(G, f):
-    G_local = G.copy()
-
-    N = len(G_local.nodes)
-    delete_amount = int(N * f)
-    remove_nodes_list = random.sample(list(G_local.nodes()), delete_amount)
-
-    for node in remove_nodes_list:
-        G_local.remove_node(node)
-    
-    return G_local
-
-# delete connected nodes
-def connected_nodes(G, f, L):          
-    G_local = G.copy()
-
-    N = len(G_local.nodes)
-    delete_amount = int(N * f)
-    count = 1
-
-    for (node, _) in L:
-        G_local.remove_node(node)
-            
-        if count >= delete_amount:
-            break
-        else:
-            count = count + 1
-
-    return G_local
-
-def generate_failure(G, remove_range):
-    diameters = []
-
-    for f in remove_range:
-        print("--------------------------")
-        print("F = " + str(f))
-        print("NODES BEFORE DELETE " + str(len(G.nodes())))
-        deleted_graph = random_nodes(G, f)
-        print("NODES AFTER DELETE " + str(len(deleted_graph.nodes())))
-        recalculated_diameter = estimate_path_length(deleted_graph, deleted_graph.nodes())
-        print("RECALCULATED DIAMETER " + str(recalculated_diameter))
-        diameters.append(recalculated_diameter)
-
-    return diameters
-
-def generate_attack(G, remove_range):
-    connected_nodes_list = get.most_connected_nodes(G) 
-    diameters = []
-
-    for f in remove_range:
-        print("--------------------------")
-        print("F = " + str(f))
-        print("NODES BEFORE DELETE " + str(len(G.nodes())))
-        modified_graph = connected_nodes(G, f, connected_nodes_list)
-        print("NODES AFTER DELETE " + str(len(modified_graph.nodes())))
-        recalculated_diameter = estimate_path_length(modified_graph, modified_graph.nodes())
-        print("RECALCULATED DIAMETER " + str(recalculated_diameter))
-        diameters.append(recalculated_diameter)
-
-    return diameters
-
-# internet topology graph - retrieved from SNAP
-print("Reading Graph...")
-facebook = read_graph('facebook_combined.txt.gz')
-
-# error tolerance
-remove_range = list(np.arange(0.005, 0.055, 0.005))
-
-# print("Error Tolerance...")
-# facebook_failure_diameters = generate_failure(facebook, remove_range)
-# print(facebook_failure_diameters)
-
-print("Attack Tolerance...")
-print("PATH LENGTH : " + str(estimate_path_length(facebook)))
-# facebook_attack_diameters = generate_attack(facebook, remove_range)
-# print(facebook_attack_diameters)
+init()
