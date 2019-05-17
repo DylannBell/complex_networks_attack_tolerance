91714f0c4ffbf851cf9b40f5ba3aa19e14fd162e
Ryan <hodgsonr1234@gmail.com>
2019-05-17 14:05:38 +0800

    failure + attack works for facebook graph - showing correct properties

--- CODE/experiment_one_extn.py
+++ CODE/experiment_one_extn.py
@@ -6,32 +6,89 @@ import numpy as np
 
 def init():
     facebook = read_graph('facebook_combined.txt.gz')
-    fb_connected_nodes = get_most_connected_nodes(facebook)
-    fb_connected_nodes = fb_connected_nodes[20:30]
+    N = len(facebook.nodes())
 
-    diameters = []
-    recursive_attack(facebook, fb_connected_nodes, diameters)
-    print(diameters)
+    # ATTACK
+    facebook_attack = facebook.copy()
+    fb_connected_nodes = get_most_connected_nodes(facebook_attack)
+    update_connected_nodes(facebook_attack, N, fb_connected_nodes)
+    filtered_connected_nodes = fb_connected_nodes[0:20]
 
-def recursive_attack(G, connected_nodes, diameters):
-        
-    if len(connected_nodes) == 0:
-        print("RETURNING NOW....")
-        print(diameters)
+    facebook_attack_diameters = []
+    recursive_attack(facebook_attack, filtered_connected_nodes, facebook_attack_diameters)
+    print(facebook_attack_diameters)
+
+    # FAILURE
+    facebook_failure = facebook.copy()
+    fb_nodes = list(facebook_failure.nodes())[0:20]
+    random.shuffle(fb_nodes)
+
+    facebook_failure_diameters = []
+    recursive_failure(facebook_failure, fb_nodes, facebook_failure_diameters)
+    print(facebook_failure_diameters)
+
+def recursive_failure(G, random_nodes, diameters):
+    # break recursion
+    if len(random_nodes) == 0:
         return diameters
     
-    print("-----------------------")
-    (node, _) = connected_nodes.pop(0)
-    print("NODE : " + str(node))
-    print("NO. OF NODES BEFORE : " + str(len(G.nodes())))
+    # remove node
+    node = random_nodes.pop(0)
     G.remove_node(node)
-    print("NO. OF NODES AFTER : " + str(len(G.nodes())))
-    L = estimate_path_length(G)
-    print("L : " + str(L))
-    diameters.append(L) 
-    print(diameters)
 
+    subgraphs = nx.connected_component_subgraphs(G)  
+    subgraph_diameters = []
+
+    for subgraph in subgraphs:
+        L = estimate_path_length(subgraph)
+        if L != 0.0:
+            subgraph_diameters.append(L)
+
+    L = max(subgraph_diameters)
+    diameters.append(L) 
+
+    # continue recursion
+    recursive_failure(G, random_nodes, diameters)
+
+def recursive_attack(G, connected_nodes, diameters):
+    # break recursion
+    if len(connected_nodes) == 0:
+        return diameters
+    
+    # remove node
+    (node, _) = connected_nodes.pop(0)
+    G.remove_node(node)
+
+    subgraphs = nx.connected_component_subgraphs(G)  
+    subgraph_diameters = []
+
+    for subgraph in subgraphs:
+        L = estimate_path_length(subgraph)
+        if L != 0.0:
+            subgraph_diameters.append(L)
+
+    L = max(subgraph_diameters)
+    diameters.append(L)
+
+    # continue recursion
     recursive_attack(G, connected_nodes, diameters)
+
+# filter the connected nodes for only those within a certain lower and upperbound
+# Maybe able to delete
+def filter_connected_nodes(connected_nodes, lower_bound, upper_bound):
+    filtered_connected_nodes = []
+
+    for (node, f) in connected_nodes:
+        if lower_bound <= f <= upper_bound:
+            print("ADDING : " + str((node, f)))
+            filtered_connected_nodes.append((node, f))
+
+    return filtered_connected_nodes
+
+def update_connected_nodes(G, N, connected_nodes):
+    for index, (node, links) in enumerate(connected_nodes):
+        fraction = round(links/N, 4)
+        connected_nodes[index] = (node, fraction)
 
 
 # average path length
@@ -65,6 +122,5 @@ def read_graph(filename):
     array = np.loadtxt(filename, dtype=int)
     G.add_edges_from(array)
     return G
-
 
 init()
